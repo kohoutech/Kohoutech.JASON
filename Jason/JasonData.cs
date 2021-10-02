cb89a4644c8419d73a0a0968f24a725564fa1f75
Kohoutech <george@kohoutech.com>
2021-10-02 16:16:57 -0400

    initial code commit

---
+++ Jason/JasonData.cs
@@ -0,0 +1,300 @@
+﻿/* ----------------------------------------------------------------------------
+Kohoutech JSON Library
+Copyright (C) 2012-2021  George E Greaney
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+----------------------------------------------------------------------------*/
+
+//JSON syntax : https://www.json.org/json-en.html
+//              https://www.ietf.org/rfc/rfc4627.txt
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+namespace Kohoutech.Jason
+{
+    public class JasonData
+    {
+        public JasonValue root;
+        public static JasonData curData;                //static field to point to jason data we're currently building
+        public Dictionary<int, Object> objects;         //dict of objs for resolving obj refs during build
+
+        public JasonData(JasonValue _root)
+        {
+            root = _root;
+            curData = this;
+            objects = new Dictionary<int, object>();
+        }
+
+        public Object buildObjectGraph()
+        {
+            Object obj = root.build();
+            return obj;
+        }
+
+        public String writeData(JasonData jd)
+        {
+            StringBuilder outText = new StringBuilder();
+            String indent = "";
+            jd.root.write(outText, indent);
+
+            return outText.ToString();
+        }
+    }
+
+    //base class
+    public class JasonValue
+    {
+        public virtual Object build()
+        {
+            return null;
+        }
+
+        public virtual void write(StringBuilder outText, String indent)
+        {
+        }
+    }
+
+    public class JasonObject : JasonValue
+    {
+        public Dictionary<String, JasonValue> members;
+
+        public JasonObject()
+        {
+            members = new Dictionary<string, JasonValue>();
+        }
+
+        public override Object build()
+        {
+            //create object from type name
+            JasonValue typeval = members["$type"];
+            String typestr = ((JasonString)typeval).str;
+            Type objType = Type.GetType(typestr);
+            Object obj = Activator.CreateInstance(objType);
+
+            //store object in objects dict by id num
+            JasonValue idval = members["$id"];
+            int idnum = (int)((JasonNumber)idval).build();
+            JasonData.curData.objects[idnum] = obj;
+
+            //iterate through field list and set values
+            foreach (String fieldname in members.Keys)
+            {
+                //skip meta fields
+                if (fieldname.StartsWith("$"))
+                    continue;
+
+                //get field value
+                JasonValue val = members[fieldname];
+                dynamic fieldval = val.build();
+
+                //if field name starts with # then it is an object reference
+                //retrieve object from objects dict by id num
+                if (fieldname.StartsWith("#"))
+                {
+                    String fname = fieldname.Substring(1);
+                    int fieldid = (int)fieldval;
+                    Object fieldobj = JasonData.curData.objects[fieldid];
+                    obj.GetType().GetField(fname).SetValue(obj, fieldobj);
+                }
+                else
+                {
+                    if (val is JasonArray)
+                    {
+                        //get the actual list that the object's field refers to
+                        FieldInfo fi = obj.GetType().GetField(fieldname);
+                        dynamic fldVal = fi.GetValue(obj);
+
+                        //add to list field one item as time
+                        List<Object> listvals = (List<Object>)fieldval;
+                        foreach (dynamic listval in listvals)
+                        {
+                            fldVal.Add(listval);
+                        }
+                    }
+                    else
+                    {
+                        obj.GetType().GetField(fieldname).SetValue(obj, fieldval);
+                    }
+                }
+            }
+
+            return obj;
+        }
+
+        public override void write(StringBuilder outText, String indent)
+        {
+            String indent2 = indent + "  ";
+            outText.Append("{\r\n" + indent2);
+            List<String> fields = new List<String>(members.Keys);
+            int idx = 1;
+            foreach (String fieldname in fields)
+            {
+                outText.Append("\"" + fieldname + "\": ");
+                JasonValue val = members[fieldname];
+                val.write(outText, indent2);
+                if (idx < fields.Count)
+                {
+                    outText.Append(",\r\n" + indent2);
+                    idx++;
+                }
+            }
+            outText.Append("\r\n" + indent + "}");
+        }
+    }
+
+    public class JasonArray : JasonValue
+    {
+        public List<JasonValue> elements;
+
+        public JasonArray()
+        {
+            elements = new List<JasonValue>();
+        }
+
+        public override Object build()
+        {
+            List<Object> vals = new List<object>();
+            foreach (JasonValue elem in elements)
+            {
+                Object val = elem.build();
+                vals.Add(val);
+            }
+            return vals;
+        }
+
+        public override void write(StringBuilder outText, String indent)
+        {
+            if (elements.Count == 0)
+            {
+                outText.Append("[]");
+            }
+            else
+            {
+                String indent2 = indent + "  ";
+                outText.Append("[\r\n" + indent2);
+                int idx = 1;
+                foreach (JasonValue elem in elements)
+                {
+                    elem.write(outText, indent2);
+                    if (idx < elements.Count)
+                    {
+                        outText.Append(",\r\n" + indent2);
+                        idx++;
+                    }
+                }
+                outText.Append("\r\n" + indent + "]");
+            }
+        }
+    }
+
+    public class JasonString : JasonValue
+    {
+        public String str;
+
+        public JasonString(String _str)
+        {
+            str = _str;
+        }
+
+        public override Object build()
+        {
+            return str;
+        }
+
+        public override void write(StringBuilder outText, String indent)
+        {
+            outText.Append("\"");
+            foreach (char c in str)
+            {
+                switch(c)
+                {
+                    case '\\' : outText.Append("\\\\"); break;
+                    
+                    default:
+                        outText.Append(c);
+                        break;
+                }
+            }
+            outText.Append("\"");
+        }
+    }
+
+    public class JasonNumber : JasonValue
+    {
+        public String num;
+
+        public JasonNumber(String _num)
+        {
+            num = _num;
+        }
+
+        public override Object build()
+        {
+            if (num.Contains("."))
+            {
+                double d = double.Parse(num);
+                return d;
+            }
+            else
+            {
+                int i = Int32.Parse(num);
+                return i;
+            }
+        }
+
+        public override void write(StringBuilder outText, String indent)
+        {
+            outText.Append(num);
+        }
+    }
+
+    public class JasonBoolean : JasonValue
+    {
+        public Boolean val;
+
+        public JasonBoolean(bool _val)
+        {
+            val = _val;
+        }
+
+        public override Object build()
+        {
+            return val;
+        }
+
+        public override void write(StringBuilder outText, String indent)
+        {
+            outText.Append(val ? "true" : "false");
+        }
+    }
+
+    public class JasonNull : JasonValue
+    {
+        public JasonNull()
+        {
+        }
+
+        //don't need to override build method
+
+        public override void write(StringBuilder outText, String indent)
+        {
+            outText.Append("null");
+        }
+    }
+}
---
+++ Jason/JasonReader.cs
@@ -0,0 +1,331 @@
+﻿/* ----------------------------------------------------------------------------
+Kohoutech JSON Library
+Copyright (C) 2012-2021  George E Greaney
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+----------------------------------------------------------------------------*/
+
+//JSON syntax : https://www.json.org/json-en.html
+//              https://www.ietf.org/rfc/rfc4627.txt
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kohoutech.Jason
+{
+    public class JasonReader
+    {
+        public JasonReader()
+        {
+        }
+
+        public Object deserialize(String jasonData)
+        {
+            JasonData jd = parseData(jasonData);
+
+            Object result = jd.buildObjectGraph();
+            return result;
+        }
+
+        //- parser ------------------------------------------------------------
+
+        String jasonStr;
+        int pos;
+        int linenum;
+        int linepos;
+
+        public JasonData parseData(string _jasonStr)
+        {
+            jasonStr = _jasonStr;
+            pos = 0;
+            linenum = 1;
+            linepos = 0;
+            JasonValue root = parseValue();
+            return new JasonData(root);
+        }
+
+        public JasonValue parseValue()
+        {
+            skipWhiteSpace();
+            if (pos == jasonStr.Length)
+            {
+                throw new JasonReadException("unexpected end of file", linenum, pos - linepos);
+            }
+
+            //parse literals first
+            if (pos < jasonStr.Length - 4)
+            {
+                if (jasonStr[pos] == 't' && jasonStr[pos + 1] == 'r' && jasonStr[pos + 2] == 'u' && jasonStr[pos + 3] == 'e')
+                {
+                    pos += 4;
+                    return new JasonBoolean(true);
+                }
+                if (jasonStr[pos] == 'n' && jasonStr[pos + 1] == 'u' && jasonStr[pos + 2] == 'l' && jasonStr[pos + 3] == 'l')
+                {
+                    pos += 4;
+                    return new JasonNull();
+                }
+            }
+            if (pos < jasonStr.Length - 5)
+            {
+                if (jasonStr[pos] == 'f' && jasonStr[pos + 1] == 'a' && jasonStr[pos + 2] == 'l' && jasonStr[pos + 3] == 's' && jasonStr[pos + 4] == 'e')
+                {
+                    pos += 5;
+                    return new JasonBoolean(false);
+                }
+            }
+
+            JasonValue val = null;
+            switch (jasonStr[pos])
+            {
+                case '{':
+                    val = parseObject();
+                    break;
+
+                case '[':
+                    val = parseArray();
+                    break;
+
+                case '\"':
+                    val = new JasonString(parseString());
+                    break;
+
+                case '-':
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                    val = new JasonNumber(parseNumber());
+                    break;
+
+                default:
+                    throw new JasonReadException("illegal char " + jasonStr[pos], linenum, pos - linepos);
+                    break;
+            }
+            return val;
+        }
+
+        public JasonObject parseObject()
+        {
+            JasonObject obj = new JasonObject();
+            pos++;                                  //skip opening delim
+
+            skipWhiteSpace();
+            if (pos < jasonStr.Length && jasonStr[pos] == '}')
+            {
+                pos++;
+                return obj;
+            }
+
+            do
+            {
+                skipWhiteSpace();
+                String str = parseString();
+                skipWhiteSpace();
+                if (pos == jasonStr.Length || jasonStr[pos++] != ':')
+                    throw new JasonReadException("missing : separator", linenum, pos - linepos);
+                JasonValue val = parseValue();
+
+                obj.members[str] = val;
+
+                skipWhiteSpace();
+                if (pos == jasonStr.Length || (jasonStr[pos] != ',' && jasonStr[pos] != '}'))
+                    throw new JasonReadException("missing , or } delimiter", linenum, pos - linepos);
+            }
+            while (jasonStr[pos++] == ',');
+            return obj;
+        }
+
+        public JasonArray parseArray()
+        {
+            JasonArray arr = new JasonArray();
+            pos++;                              //slip opening delim
+
+            skipWhiteSpace();
+            if (pos < jasonStr.Length && jasonStr[pos] == ']')
+            {
+                pos++;
+                return arr;
+            }
+            
+            do
+            {
+                skipWhiteSpace();
+                JasonValue elem = parseValue();
+                arr.elements.Add(elem);
+
+                skipWhiteSpace();
+                if (pos == jasonStr.Length || (jasonStr[pos] != ',' && jasonStr[pos] != ']'))
+                    throw new JasonReadException("missing , or ] delimiter", linenum, pos - linepos);
+            }
+            while (jasonStr[pos++] == ',');
+            return arr;
+        }
+
+        public void skipWhiteSpace()
+        {
+            while (pos < jasonStr.Length &&
+                (jasonStr[pos] == ' ' || jasonStr[pos] == '\r' || jasonStr[pos] == '\n' || jasonStr[pos] == '\t'))
+            {
+                if (jasonStr[pos] == '\n')
+                {
+                    linenum++;
+                    linepos = pos;
+                }
+                pos++;
+            }
+        }
+
+        public String parseNumber()
+        {
+            StringBuilder numstr = new StringBuilder();
+            char ch = jasonStr[pos];
+            if (ch == '-')
+            {
+                numstr.Append(ch);
+                ch = jasonStr[++pos];
+            }
+
+            if (ch == '0')
+            {
+                numstr.Append(ch);
+                ch = jasonStr[++pos];
+            }
+            else if (ch >= '1' && ch <= '9')
+            {
+                while (ch >= '0' && ch <= '9')
+                {
+                    numstr.Append(ch);
+                    ch = jasonStr[++pos];
+                }
+            }
+            else
+            {
+                throw new JasonReadException("illegal char in numeric literal " + jasonStr[pos], linenum, pos - linepos);
+            }
+
+            if (ch == '.')
+            {
+                numstr.Append(ch);
+                ch = jasonStr[++pos];
+                if (ch < '0' || ch > '9')
+                {
+                    throw new JasonReadException("illegal char in numeric fraction " + jasonStr[pos], linenum, pos - linepos);
+                }
+                while (ch >= '0' && ch <= '9')
+                {
+                    numstr.Append(ch);
+                    ch = jasonStr[++pos];
+                }
+            }
+
+            if (ch == 'e' || ch == 'E')
+            {
+                numstr.Append(ch);
+                ch = jasonStr[++pos];
+                if (ch == '+' || ch == '-')
+                {
+                    numstr.Append(ch);
+                    ch = jasonStr[++pos];
+                }
+                if (ch < '0' || ch > '9')
+                {
+                    throw new JasonReadException("illegal char in numeric exponent " + jasonStr[pos], linenum, pos - linepos);
+                }
+                while (ch >= '0' && ch <= '9')
+                {
+                    numstr.Append(ch);
+                    ch = jasonStr[++pos];
+                }
+            }
+
+            return numstr.ToString();
+        }
+
+        public String parseString()
+        {
+            StringBuilder str = new StringBuilder();
+            pos++;                                      //skip opening quote
+            while (jasonStr[pos] != '\"')
+            {
+                char ch = jasonStr[pos++];
+                if (ch == '\\')
+                {
+                    char ch2 = jasonStr[pos++];
+                    switch (ch2)
+                    {
+                        case '\"': ch = '\"'; break;
+                        case '\\': ch = '\\'; break;
+                        case '/': ch = '/'; break;
+                        case 'b': ch = '\b'; break;
+                        case 'f': ch = '\f'; break;
+                        case 'n': ch = '\n'; break;
+                        case 'r': ch = '\r'; break;
+                        case 't': ch = '\t'; break;
+                        case 'u':
+                            {
+                                int sum = 0;
+                                for (int i = 0; i < 4; i++)
+                                {
+                                    int val = 0;
+                                    char hex = ch2 = jasonStr[pos++];
+                                    if (hex >= '0' && hex <= '9')
+                                        val = hex - '0';
+                                    else if (hex >= 'A' && hex <= 'F')
+                                        val = hex - 'A';
+                                    else if (hex >= 'a' && hex <= 'f')
+                                        val = hex - 'a';
+                                    else
+                                        throw new JasonReadException("illegal char " + hex + " in hexadecimal string constant", linenum, pos - linepos);
+                                    sum = sum * 10 + val;
+                                }
+                                ch = (char)sum;
+                            }
+                            break;
+                        default:
+                            throw new JasonReadException("illegal escape char " + ch2 + " in string constant", linenum, pos - linepos);
+                            break;
+                    };
+                }
+                str.Append(ch);
+            }
+            pos++;                                      //skip closing quote
+
+            return str.ToString();
+        }
+    }
+
+    //- error handling --------------------------------------------------------
+
+    public class JasonReadException : Exception
+    {
+        int line;
+        int pos;
+
+        public JasonReadException(String reason, int _line, int _pos) : base(reason)
+        {
+            line = _line;
+            pos = _pos;
+        }
+    }
+}
